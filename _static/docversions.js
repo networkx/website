7493f70fe8128e010c8304591309ec0bc49103d0
Jarrod Millman <jarrod.millman@gmail.com>
2017-09-09 14:11:13 -0700

    Add links to archived versions

---
+++ _static/docversions.js
@@ -0,0 +1,21 @@
+var versions = ['1.10', '1.9.1', '1.9', '1.8.1', '1.8', '1.7', '1.6', '1.5', '1.4', '1.3', '1.2', '1.1', '1.0.1', '1.0', '0.99', '0.37'];
+
+function insert_version_links() {
+    for (i = 0; i < versions.length; i++){
+        open_list = '<li>'
+        if (typeof(DOCUMENTATION_OPTIONS) !== 'undefined') {
+            if ((DOCUMENTATION_OPTIONS['VERSION'] == versions[i]) ||
+                (DOCUMENTATION_OPTIONS['VERSION'].match(/latest$/) && (i == 0))) {
+                open_list = '<li id="current">'
+            }
+        }
+        document.write(open_list);
+        document.write('<a href="URL">networkx VERSION</a> </li>\n'
+                        .replace('VERSION', versions[i])
+                        .replace('URL', 'https://networkx.github.io/documentation/networkx-' + versions[i]));
+    }
+}
+
+function stable_version() {
+    return versions[1];
+}
